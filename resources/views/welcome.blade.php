acabad00ecdcf811f6fcf93374c3122f697eb575
martiresalbertoorozcogonzales <martiresalbertoorozcogonzales@gmail.com>
2021-08-25 18:45:08 -0600

    Actualizacion 25/8/21 terminado el front del portafolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                     @auth
                     <li class="mr-3">
                       <a href="{{ url('/dashboard') }}" id="navAction"
-                      class="mx-auto lg:mx-0 hover:bg-blue-300 bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+                      class="mx-auto lg:mx-0 bg-gradient-to-r from-red-500 bg-white text-gray-800 font-bold rounded-full my-5 py-3 px-7 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
                       >
                       Home
                     </a>
@@ -93,13 +93,13 @@
                     Bienvenido a mi Porftafolio
                   </h1>
                   <p class="leading-normal text-2xl mb-8">
-                   Aca encontraras toda la informacion hacerca de mi trayectoria y trabajos
+                   Aca encontraras toda la informacion acerca de mis habilidades y trabajos
                   </p>
 
                 </div>
                 <!--Right Col-->
-                <div class="w-full md:w-3/5 py-6 text-center">
-                  <img class="w-full md:w-4/5 z-50" src="img/hero.png" />
+                <div class="w-full sm:w-1/2 p-6">
+                  <img class="w-full md:w-5/5 z-50"src="img/portafolio1.svg" />
                 </div>
               </div>
             </div>
@@ -639,6 +639,139 @@
                   </div>
             </section>
 
+            <section class="bg-white border-b py-8">
+                <div class="container mx-auto flex flex-wrap pt-4 pb-12 text-center">
+
+                  <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+                    Certificaciones
+                  </h1>
+
+                  <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                    <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                      <div class="flex flex-wrap no-underline hover:no-underline">
+
+                        <div class="w-full font-bold text-xl text-gray-800 px-6">
+                          Laravel
+                        </div>
+                        <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                        </p>
+                      </div>
+
+                    </div>
+                    <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                      <div class="flex items-center justify-center">
+                          <img src="img/laravel1.jpg" width="150" height="150" alt="laravel">
+                      </div>
+                    </div>
+
+                  </div>
+                  <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                      <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                        <div class="flex flex-wrap no-underline hover:no-underline">
+
+                          <div class="w-full font-bold text-xl text-gray-800 px-6">
+                            Laravel
+                          </div>
+                          <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                          </p>
+                        </div>
+
+                      </div>
+                      <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                        <div class="flex items-center justify-center">
+                            <img src="img/laravel2.jpg" width="150" height="150" alt="laravel2">
+                        </div>
+                      </div>
+                  </div>
+                  <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                      <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                        <div class="flex flex-wrap no-underline hover:no-underline">
+
+                          <div class="w-full font-bold text-xl text-gray-800 px-6">
+                            Marketing Digital
+                          </div>
+                          <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                          </p>
+                        </div>
+
+                      </div>
+                      <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                        <div class="flex items-center justify-center">
+                            <img src="img/Marketin Digital.jpg" width="150" height="150" alt="JAVASCRIPT">
+                        </div>
+                      </div>
+                  </div>
+
+
+                    <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                      <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                        <div class="flex flex-wrap no-underline hover:no-underline">
+
+                          <div class="w-full font-bold text-xl text-gray-800 px-6">
+                            Analitica
+                          </div>
+                          <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                          </p>
+                        </div>
+
+                      </div>
+                      <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                        <div class="flex items-center justify-center">
+                            <img src="img/analitica.jpg" width="150" height="150" alt="Analitica">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                          <div class="flex flex-wrap no-underline hover:no-underline">
+
+                            <div class="w-full font-bold text-xl text-gray-800 px-6">
+                              Google Paginas
+                            </div>
+                            <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                            </p>
+                          </div>
+
+                        </div>
+                        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                          <div class="flex items-center justify-center">
+                              <img src="img/certificadogoogle.jpg" width="150" height="150" alt="Google Paginas">
+                          </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+
+                          <div class="flex flex-wrap no-underline hover:no-underline">
+
+                            <div class="w-full font-bold text-xl text-gray-800 px-6">
+                              ECOMMERCE
+                            </div>
+                            <p class="text-gray-800 text-base px-6 mb-5 pt-5">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+                            </p>
+                          </div>
+
+                        </div>
+                        <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-8">
+                          <div class="flex items-center justify-center">
+                              <img src="img/E-Commerce.jpg" width="150" height="150" alt="E-commerce">
+                          </div>
+                        </div>
+                    </div>
+
+                </div>
+              </section>
 
             <svg class="w-10 h-10" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -664,16 +797,16 @@
 
             <section class="container mx-auto text-center py-6 mb-12">
               <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
-                Call to Action
+                Enviar Mensaje
               </h1>
               <div class="w-full mb-4">
                 <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
               </div>
               <h3 class="my-4 text-3xl leading-tight">
-                Main Hero Message to sell yourself!
+                Las mejores cosas de la vida no son cosas
               </h3>
-              <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                Action!
+              <button class="mx-auto lg:mx-0 bg-gradient-to-r from-red-500 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                Enviar mensaje
               </button>
             </section>
 
@@ -681,75 +814,14 @@
             <footer class="bg-white">
               <div class="container mx-auto px-8">
                 <div class="w-full flex flex-col md:flex-row py-6">
-                  <div class="flex-1 mb-6 text-black">
+                  <div class="flex-1 mb-6 text-black text-center">
                     <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-                      <!--Icon from: http://www.potlabicons.com/ -->
-                      <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
-                        <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
-                        <path
-                          class="plane-take-off"
-                          d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-                        />
-                      </svg>
-                      LANDING
+                      Portafolio Martires Alberto Orozco
                     </a>
-                  </div>
-                  <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Links</p>
-                    <ul class="list-reset mb-6">
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">FAQ</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Help</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Support</a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Legal</p>
-                    <ul class="list-reset mb-6">
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Terms</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Privacy</a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Social</p>
-                    <ul class="list-reset mb-6">
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Linkedin</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Twitter</a>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="flex-1">
-                    <p class="uppercase text-gray-500 md:mb-6">Company</p>
-                    <ul class="list-reset mb-6">
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Official Blog</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">About Us</a>
-                      </li>
-                      <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                        <a href="#" class="no-underline hover:underline text-gray-800 hover:text-pink-500">Contact</a>
-                      </li>
-                    </ul>
+                    <p>Copyright &copy; Portafolio | Martires 2021 <a href="" target="_blank" rel="nofollow"></a></p>
                   </div>
                 </div>
               </div>
-              <a href="https://www.freepik.com/free-photos-vectors/background" class="text-gray-500">Background vector created by freepik - www.freepik.com</a>
             </footer>
             <!-- jQuery if you need it
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
