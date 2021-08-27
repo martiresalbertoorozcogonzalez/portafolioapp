b0216ee1f1eae9a2e352fa09f46477756018cf5f
martiresalbertoorozcogonzales <martiresalbertoorozcogonzales@gmail.com>
2021-08-27 07:08:37 -0600

    Actualizacion 27/8/21 Terminado diseño front de admin portafolio

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -17,38 +17,66 @@
 
                 <div class="grid grid-rows-4 grid-cols-1 gap-4 md:grid-rows-2 md:grid-cols-2">
 
-                    <a href="{{ route('portafolio.index') }}"
-                         class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20 bg-blue-700 p-4 flex flex-col relative undefined text-center hover:bg-blue-300">
+                    <a href="#"
+                         class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20  bg-gradient-to-r from-indigo-400 p-4 flex flex-col relative undefined text-center hover:bg-indigo-300">
                          <div class="absolute right-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                               </svg>
                         </div>
-                         <div class="text-lg font-medium mb-2 mt-4">Portafolio</div>
+                         <div class="text-lg mb-2 mt-4 font-bold">Habilidades</div>
                         <div class="font-normal">Aca puedes encontrar todas los trabajos hechos
                             a lo largo de mi trayectoria como progrmador.
                         </div>
                     </a>
 
                     <a href="#"
-                         class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20 bg-blue-700 p-4 flex flex-col relative undefined text-center hover:bg-blue-300">
+                         class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20  bg-gradient-to-r from-indigo-400 p-4 flex flex-col relative undefined text-center hover:bg-indigo-300">
                         <div class="absolute right-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
                             </svg>
                         </div>
-                        <div class="text-lg font-medium mb-2 mt-4">Mi perfil</div>
+                        <div class="text-lg font-bold mb-2 mt-4">Acerca de mi</div>
                         <div class="font-normal">Aca puedes encontrar la informacion de tu perfil</div>
                     </a>
 
-                    <a href="#" class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20 bg-blue-700 p-4 flex flex-col relative undefined hover:bg-blue-300">
+
+
+                    <a href=""
+                         class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20  bg-gradient-to-r from-indigo-400 p-4 flex flex-col relative undefined text-center hover:bg-indigo-300">
+                         <div class="absolute right-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+                              </svg>
+                        </div>
+                         <div class="text-lg mb-2 mt-4 font-bold">Certificaciones</div>
+                        <div class="font-normal">Aca puedes encontrar todas los trabajos hechos
+                            a lo largo de mi trayectoria como progrmador.
+                        </div>
+                    </a>
+
+                    <a href="{{ route('portafolio.index') }}"
+                        class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20  bg-gradient-to-r from-indigo-400 p-4 flex flex-col relative undefined text-center hover:bg-indigo-300">
+                        <div class="absolute right-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+                                </svg>
+                        </div>
+                        <div class="text-lg mb-2 mt-4 font-bold">Portafolio</div>
+                        <div class="font-normal">Aca puedes encontrar todas los trabajos hechos
+                            a lo largo de mi trayectoria como progrmador.
+                        </div>
+                    </a>
+
+                    <a href="#" class="rounded-2x backdrop-filter backdrop-blur-lg bg-opacity-20  bg-gradient-to-r from-indigo-400 p-4 flex flex-col relative undefined text-center hover:bg-indigo-300">
                         <div class="absolute right-2">
                               <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
                               </svg>
                             </div>
 
-                        <div class="text-lg font-medium mb-2 mt-4">Mensajes</div>
+                        <div class="text-lg font-bold mb-2 mt-4">Contactame</div>
                         <div class="font-normal">Writing the actual code for the site, as it controls everything
                             that happens behind the scenes of the site.</div>
                     </a>
--- resources/views/admin/portafolio/index.blade.php
+++ resources/views/admin/portafolio/index.blade.php
@@ -4,27 +4,29 @@
 
 <div class="container px-4 mx-auto mt-6">
 
-    <a href="{{ route('portafolio.create') }}" class="bg-blue-500  hover:bg-blue-400 p-2 mb-2 rounded" type="submit">Crear Portafolio</a>
+    <a href="{{ route('portafolio.create') }}" class="bg-gradient-to-l from-red-600  hover:bg-indigo-700 p-2 mb-2 rounded" type="submit">Crear Portafolio</a>
 
 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
 
 <table class="w-full divide-y divide-gray-500 table-auto">
 
-    <thead class="bg-gray-300">
+    <thead class="bg-indigo-600">
       <tr>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Title</th>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Author</th>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Categoria</th>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Imagen</th>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Editar</th>
-        <th class="px-6 py-3 text-lg font-medium text-gray-500">Eliminar</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Id</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Title</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Author</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Categoria</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Imagen</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Editar</th>
+        <th class="px-6 py-3 text-lg font-medium text-white">Eliminar</th>
     </tr>
     </thead>
 
     <tbody class="divide-y divide-gray-200">
     @foreach ($portafolio as $portafolio)
-      <tr class="text-center bg-white  hover:bg-gray-200">
+      <tr class="text-center bg-white">
+        <td class="text-center px-6 py-3">{{ $portafolio->id }}</td>
         <td class="text-center px-6 py-3">{{ $portafolio->titulo }}</td>
         <td class="text-center px-6 py-3">{{ $portafolio->autor->name }}</td>
         <td class="text-center px-6 py-3">{{ $portafolio->categoria->nombre }}</td>
@@ -32,13 +34,13 @@
             <img src="/storage/{{$portafolio->imagen}}" style="width:200px;">
         </td>
         <td>
-            <a href="{{ route('portafolio.edit',['portafolio' => $portafolio->id]) }}" class="bg-blue-500  hover:bg-blue-400 p-4 rounded" type="submit">Editar</a>
+            <a href="{{ route('portafolio.edit',['portafolio' => $portafolio->id]) }}" class="bg-gradient-to-l from-indigo-600 hover:bg-red-500 p-4 rounded" type="submit">Editar</a>
         </td>
         <td class="px-6 py-3">
             <form action="{{ route('portafolio.destroy', ['portafolio' => $portafolio->id]) }}" method="post">
                 @csrf
                 @method('DELETE')
-                <button class="bg-red-500  hover:bg-red-400 p-4 rounded" type="submit">Eliminar</button>
+                <button class="bg-gradient-to-l from-red-500  hover:bg-indigo-600 p-4 rounded" type="submit">Eliminar</button>
             </form>
         </td>
       </tr>
