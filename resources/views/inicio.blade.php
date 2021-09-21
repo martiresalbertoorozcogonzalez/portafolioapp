5c7fb4d268820d9a6975e4f4d617cf4cc805b153
martiresalbertoorozcogonzales <martiresalbertoorozcogonzales@gmail.com>
2021-09-20 22:09:12 -0600

    Actualizacion 20/09/2021 creada la ventana modal de habilidades

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -23,6 +23,8 @@
                 <p class="text-lg leading-relaxed">
                     {!! $acerca->descripcion !!}
                 </p>
+
+
                 <div class="inline-flex spacex4 text-5xl text-gray-600">
                 <ion-icon name="logo-github"></ion-icon>
                 <ion-icon name="logo-linkedin"></ion-icon>
@@ -32,6 +34,7 @@
             <img src="/storage/{{$acerca->imagen}}" alt="" class="w-80 rounded-full border-6 border-blue-300 shadow-md flex-shrink-0">
             @endforeach 
         </div>
+
     </div>
 </section>
 
@@ -52,6 +55,9 @@
          <img src="/storage/{{$habilidad->imagen}}" class="w-8 h-8" alt="">   
          <h3 class="mt-3 text-2xl font-bold text-gray-800">{{ $habilidad->nombre }}</h3>
          <p class="mt-4">{!! $habilidad->descripcion !!}</p>
+
+         <button class="modal-open boton-portafolio">Ves mas</button>
+
         </div>
       </div>
     @endforeach
@@ -72,7 +78,47 @@
          <p class="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, tenetur numquam!</p>
         </div>
       </div> -->
-      
+       <!--Modal-->
+       <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+        
+        <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+          
+          <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+            <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+              <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+            </svg>
+            <span class="text-sm">(Esc)</span>
+          </div>
+
+          <!-- Add margin if you want to see some of the overlay behind the modal-->
+          <div class="modal-content py-4 text-left px-6">
+            <!--Title-->
+            <div class="flex justify-between items-center pb-3">
+              <p class="text-2xl font-bold">Simple Modal!</p>
+              <div class="modal-close cursor-pointer z-50">
+                <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                  <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                </svg>
+              </div>
+            </div>
+
+            <!--Body-->
+            <p>Modal content can go here</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+            <p>...</p>
+
+            <!--Footer-->
+            <div class="flex justify-end pt-2">
+              <button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
+              <button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
       
     </div>
 </section>  
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -27,31 +27,52 @@
 
 
 @yield('content')
+  
 
 
 <script>
-       // Grabs all the Elements by their IDs which we had given them
-       let modal = document.getElementById("my-modal");
-       let btn = document.getElementById("open-btn");
-       let button = document.getElementById("ok-btn");
-
-        // We want the modal to open when the Open button is clicked
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
-        
-        // We want the modal to close when the OK button is clicked
-        button.onclick = function() {
-        modal.style.display = "none";
-        }
-
-        // The modal will close when the user clicks anywhere outside the modal
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
+  var openmodal = document.querySelectorAll('.modal-open')
+  for (var i = 0; i < openmodal.length; i++) {
+    openmodal[i].addEventListener('click', function(event){
+      event.preventDefault()
+      toggleModal()
+    })
+  }
+  
+  const overlay = document.querySelector('.modal-overlay')
+  overlay.addEventListener('click', toggleModal)
+  
+  var closemodal = document.querySelectorAll('.modal-close')
+  for (var i = 0; i < closemodal.length; i++) {
+    closemodal[i].addEventListener('click', toggleModal)
+  }
+  
+  document.onkeydown = function(evt) {
+    evt = evt || window.event
+    var isEscape = false
+    if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc")
+    } else {
+      isEscape = (evt.keyCode === 27)
+    }
+    if (isEscape && document.body.classList.contains('modal-active')) {
+      toggleModal()
+    }
+  };
+  
+  
+  function toggleModal () {
+    const body = document.querySelector('body')
+    const modal = document.querySelector('.modal')
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+    body.classList.toggle('modal-active')
+  }
+  
+   
 </script>
+
+
 </body>
 </html>
 
